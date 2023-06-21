d929f5f4b4f0d4fd3630e4c9cad02b019e476252
Clara Lopez <75169643+clara-s-lopez@users.noreply.github.com>
2023-06-21 04:16:41 -0700

    Menu no longer displaces scroll position!

--- it161/fp/js/main.js
+++ it161/fp/js/main.js
@@ -7,3 +7,16 @@ document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.or
  let d = new Date(); let thisYear = d.getFullYear();
  document.getElementById("this-year").innerHTML = thisYear;
 
+//Moves overlay containing slide menu to the front
+ document.getElementById('menu-toggle').addEventListener('change', function() {
+    const overlay = document.getElementById('overlay');
+    if (this.checked) {
+      overlay.style.zIndex = '9999';
+    } else {
+      // Delay the restoration of zIndex by 0.3 seconds
+    setTimeout(function() {
+        overlay.style.zIndex = 'initial';
+      }, 300);
+    }
+  });
+  
